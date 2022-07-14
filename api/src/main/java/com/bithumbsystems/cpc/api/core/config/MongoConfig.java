d46a388285741142ab2d2a0893b2a9bfe4b0feef
seongjinna <seongjinna@gmail.com>
2022-07-14 22:29:01 +0900

    documentdb -> mongodb로 변경

--- api/src/main/java/com/bithumbsystems/cpc/api/core/config/MongoConfig.java
+++ api/src/main/java/com/bithumbsystems/cpc/api/core/config/MongoConfig.java
@@ -2,6 +2,7 @@ package com.bithumbsystems.cpc.api.core.config;
 
 import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
+import com.mongodb.connection.netty.NettyStreamFactoryFactory;
 import com.mongodb.reactivestreams.client.MongoClient;
 import com.mongodb.reactivestreams.client.MongoClients;
 import java.io.ByteArrayInputStream;
@@ -56,58 +57,17 @@ public class MongoConfig extends AbstractReactiveMongoConfiguration {
 
     @SneakyThrows
     protected MongoClientSettings configureClientSettings() {
-        MongoClientSettings.Builder builder = MongoClientSettings.builder();
-        builder.applyConnectionString(getConnectionString());
-
-        var endOfCertificateDelimiter = "-----END CERTIFICATE-----";
-        //File resource = resourceLoader.getResource("classpath:cert/rds-combined-ca-bundle.pem").getFile();  // new ClassPathResource("cert/rds-combined-ca-bundle.pem").getFile();
-
-        ClassPathResource resources = new ClassPathResource("cert/rds-combined-ca-bundle.pem");
-        InputStream inputStream = resources.getInputStream();
-        File resource = File.createTempFile("rds-combined-ca-bundle", "pem");
-
-        try {
-            FileUtils.copyInputStreamToFile(inputStream, resource);
-        }finally {
-            inputStream.close();
-        }
-
-        String pemContents = new String(Files.readAllBytes(resource.toPath()));
-        var allCertificates = Arrays.stream(pemContents
-                .split(endOfCertificateDelimiter))
-            .filter(line -> !line.isBlank())
-            .map(line -> line + endOfCertificateDelimiter)
-            .collect(Collectors.toUnmodifiableList());
-
-        var certificateFactory = CertificateFactory.getInstance("X.509");
-        var keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
-        // This allows us to use an in-memory key-store
-        keyStore.load(null);
-
-        for (int i = 0; i < allCertificates.size(); i++) {
-            var certString = allCertificates.get(i);
-            var caCert = certificateFactory.generateCertificate(new ByteArrayInputStream(certString.getBytes()));
-            keyStore.setCertificateEntry(String.format("AWS-certificate-%s", i), caCert);
-        }
-
-        var trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
-        trustManagerFactory.init(keyStore);
-
-        var sslContext = SSLContext.getInstance("TLS");
-        sslContext.init(null, trustManagerFactory.getTrustManagers(), null);
-
-        builder.applyToSslSettings(ssl -> ssl.enabled(true).context(sslContext));
-
-        return builder.build();
+        return MongoClientSettings.builder()
+            .streamFactoryFactory(NettyStreamFactoryFactory.builder().build())
+            .applyConnectionString(getConnectionString())
+            .build();
     }
-
     private ConnectionString getConnectionString() {
-        String str = String.format("mongodb://%s:%s@%s:%s/%s?replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false",
+        String str = String.format("mongodb://%s:%s@%s:%s",
             config.getMongoProperties().getMongodbUser(),
             config.getMongoProperties().getMongodbPassword(),
             config.getMongoProperties().getMongodbUrl(),
-            config.getMongoProperties().getMongodbPort(),
-            config.getMongoProperties().getMongodbName()
+            config.getMongoProperties().getMongodbPort()
         );
 
         return new ConnectionString(str);
