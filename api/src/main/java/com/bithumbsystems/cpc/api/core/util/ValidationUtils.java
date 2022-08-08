93526849139e5132a44082b9b13ac565c0ee5aa1
seongjinna <seongjinna@gmail.com>
2022-08-08 11:12:03 +0900

    https://find-sec-bugs.github.io/bugs.htm#REDOS

--- api/src/main/java/com/bithumbsystems/cpc/api/core/util/ValidationUtils.java
+++ api/src/main/java/com/bithumbsystems/cpc/api/core/util/ValidationUtils.java
@@ -10,7 +10,7 @@ import org.springframework.util.StringUtils;
 public class ValidationUtils {
 
   public static void assertEmailFormat(String value) throws InvalidParameterException {
-    String regexp = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
+    String regexp = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$";
     Pattern pattern = Pattern.compile(regexp);
     Matcher matcher = pattern.matcher(value);
     if (!matcher.matches()) {
