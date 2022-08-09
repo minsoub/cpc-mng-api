9594180f78bd428ce34a58b9065303e55604b011
seongjinna <seongjinna@gmail.com>
2022-08-09 18:54:06 +0900

    미사용 메소드 제거

--- api/src/main/java/com/bithumbsystems/cpc/api/core/util/ValidationUtils.java
+++ api/src/main/java/com/bithumbsystems/cpc/api/core/util/ValidationUtils.java
@@ -3,38 +3,9 @@ package com.bithumbsystems.cpc.api.core.util;
 import com.bithumbsystems.cpc.api.core.exception.InvalidParameterException;
 import com.bithumbsystems.cpc.api.core.model.enums.ErrorCode;
 import java.util.Arrays;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import org.springframework.util.StringUtils;
 
 public class ValidationUtils {
-
-  public static void assertEmailFormat(String value) throws InvalidParameterException {
-    String regexp = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$";
-    Pattern pattern = Pattern.compile(regexp);
-    Matcher matcher = pattern.matcher(value);
-    if (!matcher.matches()) {
-      throw new InvalidParameterException(ErrorCode.INVALID_EMAIL_FORMAT);
-    }
-  }
-
-  public static void assertNameFormat(String value) throws InvalidParameterException {
-    String regexp = "^[a-zA-Z0-9가-힣. ]*$";
-    Pattern pattern = Pattern.compile(regexp);
-    Matcher matcher = pattern.matcher(value);
-    if (!matcher.matches()) {
-      throw new InvalidParameterException(ErrorCode.INVALID_NAME_FORMAT);
-    }
-  }
-
-  public static void assertCellPhoneFormat(String value) throws InvalidParameterException {
-    String regexp = "^01(?:0|1|[6-9])[.-]?(\\d{3}|\\d{4})[.-]?(\\d{4})$";
-    Pattern pattern = Pattern.compile(regexp);
-    Matcher matcher = pattern.matcher(value);
-    if (!matcher.matches()) {
-      throw new InvalidParameterException(ErrorCode.INVALID_PHONE_FORMAT);
-    }
-  }
 
   public static void assertAllowFileExt(String fileName, String[] allowExt) {
     String ext = StringUtils.getFilenameExtension(fileName).toUpperCase();
